6621cfd62123aad06ed8cce7f10fb8ac156172e6
Samu-Eng <you@example.com>
2023-01-27 20:36:29 -0300

    TiposVariasveis

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        System.out.println("Fim");
+
+
+
+
+        
+        
     }
 }
